fd68fcf80dbb56021e0a60a938c83b63f38a3c78
kekerhang <kekerhang@gmail.com>
2017-04-29 10:28:28 +0800

    Create osumo.php

---
+++ kekerhang/2017.4.29First/osumo.php
@@ -0,0 +1,14 @@
+<html>
+
+<body>
+ <h1>oooooooooooo</h1>
+ <?php
+  echo "<h2>Hi!</h2>";
+  $sum=0;
+  for($i=1;$i<=100;$i++)
+    $sum+=$i;
+  echo "1+2+...+100 = "."<font color='blue'>".$sum."</font>";
+  echo "<h3><ins>ByeBye!</ins></h3>";
+ ?>
+</body>
+</html>
